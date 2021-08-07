6177d8809674b2bd7eec57c227df903fbf2c2ff8
kazue kitamura <73727763+kazuekitamura@users.noreply.github.com>
2021-08-07 18:35:45 +0900

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import axios from "axios";
 import "./styles.css";
+import { useState } from "react";
+import { Todo } from "./Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
   const onCliskFrtchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onCliskFrtchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
