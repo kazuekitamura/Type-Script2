6c2d74ec684b07d2fcb6993ceda3a1ac65c462be
kazue kitamura <73727763+kazuekitamura@users.noreply.github.com>
2021-08-07 19:06:37 +0900

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import axios from "axios";
-import "./styles.css";
 import { useState } from "react";
+import "./styles.css";
 import { Todo } from "./Todo";
 
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
-
 export default function App() {
-  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const [todos, setTodos] = useState<any>([]);
+
   const onCliskFrtchData = () => {
-    axios
-      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => {
-        setTodos(res.data);
-      });
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
   };
   return (
     <div className="App">
       <button onClick={onCliskFrtchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userId} />
+        <Todo title={todo.title} userif={todo.userid} />
       ))}
     </div>
   );
